2e622b8ec98e4c22ada5e40cb1a972a9939e6adc
muazam mughal <104276759+MuazamMughal@users.noreply.github.com>
2025-04-28 03:56:44 +0500

    url path multiple routs and prams

--- 01_hello_who's/01_url_path/hello.py
+++ 01_hello_who's/01_url_path/hello.py
@@ -6,3 +6,8 @@ app :FastAPI = FastAPI()
 def urlpath(name: str):
     return {"message": f"Hello {name}!"}
 
+# now amnking another rout in the same file
+
+@app.get("/hello/{name}/age/{age}")
+def helloname_age(name:str , age:int):
+    return {"message": f"Hello {name}!, your age is {age}"}
