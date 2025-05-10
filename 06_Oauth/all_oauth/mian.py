c4e1ec5fd6cfe31389332b2cf9d7c1a787c91180
muazam mughal <104276759+MuazamMughal@users.noreply.github.com>
2025-05-10 14:45:20 +0500

    oauth

--- 06_Oauth/all_oauth/mian.py
+++ 06_Oauth/all_oauth/mian.py
@@ -95,3 +95,20 @@ async def get_current_active_user(current_user: UserInDB = Depends(get_current_u
     if current_user.disabled:
         raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
+
+
+
+@app.post("/token", response_model=Token)
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_date=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
