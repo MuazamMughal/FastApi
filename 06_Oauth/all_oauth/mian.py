965d76506cd5153f365af058f5ad3f2fee6e8c69
muazam mughal <104276759+MuazamMughal@users.noreply.github.com>
2025-05-10 14:51:56 +0500

    oauth

--- 06_Oauth/all_oauth/mian.py
+++ 06_Oauth/all_oauth/mian.py
@@ -112,3 +112,7 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends()):
         data={"sub": user.username}, expires_date=access_token_expires
     )
     return {"access_token": access_token, "token_type": "bearer"}
+
+@app.get("/users/me", response_model=User)
+async def read_users_me(current_user: User = Depends(get_current_active_user)):
+    return current_user
