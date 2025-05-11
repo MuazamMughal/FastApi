52bfc9cfea9836db0e456accce8357ed6e2a1273
muazam mughal <104276759+MuazamMughal@users.noreply.github.com>
2025-05-11 12:51:48 +0500

    oauth

--- 06_Oauth/all_oauth/mian.py
+++ 06_Oauth/all_oauth/mian.py
@@ -4,6 +4,16 @@ from pydantic import BaseModel
 
 
 #first i have to make fake database
+db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johando@gmail.com",
+        "hashed_password": "$2b$12$KIX3v0x1Q4Z5J6g7Y8e5Oe9j1qz5f5F5F5F5F5F5F5F5F5F5F5F",
+        "disabled": False,
+    }
+}
+
 
 
 
