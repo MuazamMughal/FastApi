346731a6c3a186d68e3e05027932606d06a30bbc
muazam mughal <104276759+MuazamMughal@users.noreply.github.com>
2025-05-13 09:05:57 +0500

    oauth helloworld simple oauth

--- 06_Oauth/00_hellowrold/03_simple_oauth/main.py
+++ 06_Oauth/00_hellowrold/03_simple_oauth/main.py
@@ -73,3 +73,24 @@ async def get_current_active_user(
     if current_user.disabled:
         raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
+
+
+
+@app.post("/token")
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    user = UserInDB(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    return {"access_token": user.username, "token_type": "bearer"}
+
+
+@app.get("/users/me")
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_active_user)]
+):
+    return current_user
