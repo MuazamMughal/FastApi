5344e1b6a2c60900edb1c8e91028fb28d88d243f
muazam mughal <104276759+MuazamMughal@users.noreply.github.com>
2025-05-26 08:20:48 +0500

    authentication

--- 06_Oauth/02_jwt_auth/04_authenticatin/src/utils/helpers.py
+++ 06_Oauth/02_jwt_auth/04_authenticatin/src/utils/helpers.py
@@ -14,3 +14,24 @@ pwd_context = CryptContext(schemes=["sha256_crypt"], deprecated="auto")
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
+
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+class InvalidUserException(Exception):
+    """
+    Exception raised when a user is not found in the database or if the user data is invalid.
+    """
+    def __init__(self, status_code: int, detail: str):
+        self.status_code = status_code
+        self.detail = detail
+        super().__init__(detail)
+
+# Create a custom credentials exception
+credentials_exception = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    headers={"WWW-Authenticate": 'Bearer'},
+    detail={"error": "invalid_token", "error_description": "The access token expired"}
+)
