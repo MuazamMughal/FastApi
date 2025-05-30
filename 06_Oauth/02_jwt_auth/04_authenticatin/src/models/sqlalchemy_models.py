a49a367b5db3a94772c0283eec2da8c1c532093a
muazam mughal <104276759+MuazamMughal@users.noreply.github.com>
2025-05-30 08:31:01 +0500

    authentication

--- 06_Oauth/02_jwt_auth/04_authenticatin/src/models/sqlalchemy_models.py
+++ 06_Oauth/02_jwt_auth/04_authenticatin/src/models/sqlalchemy_models.py
@@ -15,3 +15,25 @@ class RoleEnum(enum.Enum):
 
 class Base(DeclarativeBase):
     pass
+
+
+class USER(Base):
+    """
+    Represents a User in the database.
+    """
+    __tablename__ = "users_table"
+
+    id: Mapped[UUID] = mapped_column(
+        UUID, primary_key=True, index=True, default=uuid.uuid4)
+    email: Mapped[str] = mapped_column(String, unique=True, index=True)
+    username: Mapped[str] = mapped_column(String, unique=True, index=True)
+    full_name: Mapped[str] = mapped_column(String)
+    hashed_password: Mapped[str] = mapped_column(String, index=True)
+    email_verified: Mapped[bool] = mapped_column(Boolean, default=False)
+    role: Mapped[RoleEnum] = mapped_column(Enum(RoleEnum), default=RoleEnum.user)
+    disabled: Mapped[bool] = mapped_column(Boolean, default=False)
+
+    created_at: Mapped[datetime.datetime] = mapped_column(
+        DateTime, default=lambda: datetime.datetime.now(datetime.timezone.utc))
+    updated_at: Mapped[datetime.datetime] = mapped_column(
+        DateTime, default=lambda: datetime.datetime.now(datetime.timezone.utc), onupdate=lambda: datetime.datetime.now(datetime.timezone.utc))
